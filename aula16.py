558dd5ed1f3dbfc4ccde31e786634c638d86f647
Mauricio Miranda <mauriciodesouzamiranda@gmail.com>
2023-05-03 22:13:50 -0300

    Aulas 16, 17 e 18

--- aula16.py
+++ aula16.py
@@ -4,7 +4,11 @@ entrada = input('Você quer "entrar" ou "sair"? ')
 
 if entrada == 'entrar':
     print('Você entrou no sistema')
+
+    print(12341234)
 elif entrada == 'sair':
     print('Você saiu do sistema')
 else:
     print('Você não digitou nem entrar e nem sair.')
+
+print('FORA DOS BLOCOS')
